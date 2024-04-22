e33ad59c4c9dddec605fcd8a6ad45333a9cbfc2b
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-04-22 20:47:14 +0200

    Fixed the ternary operator by replacing the semi-colon with a colon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TASK: import helper functions from utils
 // TASK: import initialData
 import { getTasks, createNewTask, patchTask, putTask, deleteTask } from ".utils/taskFunction.js";
-import initialData from ".initialData.js";
+import { initialData } from ".initialData.js";
 
 /*************************************************************************************************************************************************
  * FIX BUGS!!!
@@ -32,7 +32,7 @@ function fetchAndDisplayBoardsAndTasks() {
   displayBoards(boards);
   if (boards.length > 0) {
     const localStorageBoard = JSON.parse(localStorage.getItem("activeBoard"))
-    activeBoard = localStorageBoard ? localStorageBoard ; boards[0];
+    activeBoard = localStorageBoard ? localStorageBoard : boards[0];
     elements.headerBoardName.textContent = activeBoard
     styleActiveBoard(activeBoard)
     refreshTasksUI();
