893e47fb6f791c9d01bf1977d781c880b37901c9
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-04-23 14:52:29 +0200

    Fixed the click event syntax for the 'showSideBarBtn' element

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ function setupEventListeners() {
 
   // Show sidebar event listener
   elements.hideSideBarBtn.addEventListener('click', () => toggleSidebar(false));
-  elements.showSideBarBtn.click() => toggleSidebar(true));
+  elements.showSideBarBtn.addEventListener('click', () => toggleSidebar(true));
 
   // Theme switch event listener
   elements.themeSwitch.addEventListener('change', toggleTheme);
