6a1874cfa8ad908fa62790b69144030f6617ff39
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-04-24 15:46:41 +0200

    Included the 'selectStatus' user input so that the user could choose the status of the task

--- index.js
+++ index.js
@@ -331,7 +331,7 @@ function saveTaskChanges(taskId) {
   const task_id = JSON.parse(localStorage.getItem('id')); // Fetches task ID from local storage
   const titleInput = elements.editTaskTitleInput.value; // Fetches current value entered that aloows users to input/edit title of a task 
   const descriptionInput = elements.editTaskDescInput.value; // Allows users to input/edit description or details of a task 
-
+  const selectStatus = elements.editSelectStatus.value; // Chooses the status of the task
 
   // Create an object with the updated task details
 
