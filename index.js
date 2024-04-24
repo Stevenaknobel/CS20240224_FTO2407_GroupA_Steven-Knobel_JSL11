2a1d822e8479079c1e7d1cbd67470f7d240a6f6a
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-04-24 15:15:41 +0200

    Included an event listener that triggers deletion of specified task 'deleteTask(task.id)', while also hiding the task modal, and then refreshing the tasks UI

--- index.js
+++ index.js
@@ -315,7 +315,11 @@ function openEditTaskModal(task) {
   });
 
   // Delete task using a helper function and close the task modal
-
+  deleteTaskBtn.addEventListener('click', () => {
+    deleteTask(task.id);
+    toggleModal(false, elements.editTaskModal);
+    refreshTasksUI();
+  })
 
   toggleModal(true, elements.editTaskModal); // Show the edit task modal
 }
